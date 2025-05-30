692a961ffcc8751b4fe3a1a22973751e0f39c92f
chensoul <ichensoul@gmail.com>
2025-05-30 13:59:19 +0800

    build: 更新 Spring Boot 版本并移除观测性模块
    - 将 Spring Boot 版本从 3.4.3 升级到 3.4.6
    - 移除了多个观测性相关模块的代码和配置
    - 删除了多个示例应用和控制器
    - 更新了多个模块的依赖版本

---
+++ spring-boot-observability/README.md
@@ -0,0 +1,8 @@
+# spring-boot-observability
+
+![Diagram](./gif/diagram.gif)
+
+
+# Links
+
+- https://github.com/nlinhvu/hello-service/
---
+++ spring-boot-observability/src/test/java/com/chensoul/helloservice/HelloServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chensoul.helloservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
